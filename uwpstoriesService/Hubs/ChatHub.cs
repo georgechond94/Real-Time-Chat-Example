24a7caf9f721dfa50b76e56fd7a53b11e6bb36c3
George Chondrompilas <georgechond94@outlook.com>
2016-02-20 21:10:11 +0200

    Changed folder/namespace name of ChatHub

---
+++ uwpstoriesService/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using uwpstoriesService.DataObjects;
+
+namespace uwpstoriesService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(ChatMessage message)
+        {
+            Clients.All.broadcastMessage(message);
+        }
+    }
+}
